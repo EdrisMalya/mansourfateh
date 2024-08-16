8e2529efaee69cd04e3c4d60e0ba9e777663d73c
Ahmad Edris Malia <adrismalya@gmail.com>
2024-08-16 16:17:44 +0200

    New chages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,13 @@ import Banner from "@/components/banner";
 import ComponentOne from "@/components/ComponentOne";
 import ComponentTwo from "@/components/component-two";
 import ContactComponent from "@/components/contact-component";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 export default function Home() {
   return (
--- src/components/ComponentOne.tsx
+++ src/components/ComponentOne.tsx
@@ -1,9 +1,6 @@
 "use client";
 import React, { useState } from "react";
 import Link from "next/link";
-import Image from "next/image";
-import clsx from "clsx";
-import { ArrowLeftIcon, ArrowRightIcon } from "lucide-react";
 import {
   Carousel,
   CarouselContent,
